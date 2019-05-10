f35a55fd59d1b3263ccde0678b9f066e1f200a99
lixin <slixina@sina.com>
2019-05-10 11:14:16 +0800

    初始化

---
+++ src/pages/schedule/scheduleConfirm/scheduleConfirm.jsx
@@ -0,0 +1,28 @@
+import Taro from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import _fetch from '@/utils/fetch.js'
+
+class ScheduleConfirm extends Taro.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+           
+        }
+    } 
+
+
+    componentDidMount () {
+
+    }
+    
+    render() {
+        // let { master_data } = this.state
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+export default ScheduleConfirm
---
+++ src/pages/schedule/scheduleHeader/scheduleHeader.jsx
@@ -0,0 +1,24 @@
+import Taro from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import _fetch from '@/utils/fetch.js'
+
+class ScheduleHeader extends Taro.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+           
+        }
+    } 
+
+    
+    render() {
+        // let { master_data } = this.state
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+export default ScheduleHeader
