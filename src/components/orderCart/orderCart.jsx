ed1fb519f897788c20dd2a857b5bf13bc3a5a858
ljq <534873014@qq.com>
2019-05-15 14:04:05 +0800

    ljq

--- src/components/orderCart/orderCart.jsx
+++ src/components/orderCart/orderCart.jsx
@@ -14,11 +14,12 @@ class OrderCart extends Taro.Component {
 	}
    
 	render () {
+        let { order_no, reserve, } = this.props.orderInfo
 		return (
 			<View className={style.orderCartWrap}>
                 <View className={style.orderHead}>
                     <View>订单号:</View>
-                    <View>190218111112</View>
+                    <View>{order_no}</View>
                     <Button className={style.copyBtn}>复制</Button>
                 </View>
                 <View className={style.userInfo}>
@@ -27,7 +28,7 @@ class OrderCart extends Taro.Component {
                             className={style.customerAvatar}
                             src={customerAvatar}
                         />
-                        <View className={style.customerName}>程程</View>
+                        <View className={style.customerName}>{reserve.name}</View>
                     </View>
                     <Button className={style.detailBtn}>详情</Button>
                 </View>
@@ -37,11 +38,11 @@ class OrderCart extends Taro.Component {
                         className={style.serverIco}
                         // src={}
                     />
-                    事业
+                    {reserve.service_name}
                 </View>
                 <View className={style.dateTime}>
                     预约时间:
-                    <Text className={style.time}>2019-02-18  星期一  08:00</Text>
+                    <Text className={style.time}>{reserve.schedule_code}</Text>
                     
                 </View>
                 <View className={style.process}>
--- src/pages/reserve/reserve.jsx
+++ src/pages/reserve/reserve.jsx
@@ -2,6 +2,8 @@ import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 import _fetch from '@/utils/fetch.js'
+import checkLogin from '@/utils/checkLogin.js'
+
 
 import OrderCart from '@/components/orderCart/orderCart'
 import HeaderTitle from '@/components/headerTitle/headerTitle'
@@ -16,6 +18,7 @@ class Reserve extends Taro.Component {
 		super(props)
 		this.state = { 
 			current: 0,
+			orderList: []
         }
 	}
 	
@@ -28,17 +31,22 @@ class Reserve extends Taro.Component {
     init () {
         _fetch({url:'/masterin/reserve_list',payload: null,method: 'POST',autoLogin:false, showToast: false})
         .then(res=>{
-            console.log(res)
+			console.log(res)
+			this.setState({
+				orderList: res
+			})
         })
-    }
-    
+	}
+	
     componentDidMount () {
+        checkLogin();
         this.init();
     }
 
     
 	render () {
 		const tabList = [{ title: '准备服务' }, { title: '暂待联系' }]
+		let { orderList } = this.state 
 		return (
 			<View className={style.reserveWrap}>
                 <HeaderTitle
@@ -47,9 +55,12 @@ class Reserve extends Taro.Component {
 				<AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick.bind(this)}>
 					<AtTabsPane current={this.state.current} index={0} >
 						<View className={style.orderList}>
-                            <View className={style.orderCattItem}>
-                                <OrderCart/>
-                            </View>
+							{orderList.map((item)=>
+								<View className={style.orderCattItem} key={item.id}>
+									<OrderCart orderInfo={item}/>
+								</View>
+							)}
+                            
                         </View>
 					</AtTabsPane>
 					<AtTabsPane current={this.state.current} index={1}>
